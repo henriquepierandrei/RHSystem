e81a79a44ba9822a02a292a525f8bdc73dfe3c2b
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2024-10-02 13:15:36 -0300

    Initial commit

---
+++ src/test/java/com/pierandrei/RHSystem/RhSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierandrei.RHSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
