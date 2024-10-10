b78c32a67f1ffd8ca16d8eed05eb003c851e7b4e
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2024-10-10 14:42:35 -0300

    add novas funcionalidades

--- src/main/java/com/pierandrei/RHSystem/repository/VacationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pierandrei.RHSystem.repository;
-
-import com.pierandrei.RHSystem.model.EmployeeModels.VacationModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VacationRepository extends JpaRepository<VacationModel, Long> {
-}
