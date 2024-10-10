b6cb2d1c4c13a132c7447a054e2ea8376a299a34
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2024-10-10 14:52:48 -0300

    add novas funcionalidades

--- src/main/java/com/pierandrei/RHSystem/RhSystemApplication.java
+++ src/main/java/com/pierandrei/RHSystem/RhSystemApplication.java
@@ -2,8 +2,10 @@ package com.pierandrei.RHSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RhSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pierandrei/RHSystem/scheduled/ScheduledService.java
+++ src/main/java/com/pierandrei/RHSystem/scheduled/ScheduledService.java
@@ -9,12 +9,13 @@ import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
-public class SchudeledService {
+public class ScheduledService {
     private final AdminService adminService;
 
 
     @Scheduled(cron = "0 59 23 * * *") // Executa às 23:59 todos os dias
     public void removeLeave() {
+        System.out.println("Removendo funcionários da licença!");
         this.adminService.removeLeave(LocalDate.now());
     }
 }
